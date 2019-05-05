4335e97947df4f0058e889d25e58e0d95c285a1d
DRTAG <masan.korea@gmail.com>
2019-05-05 09:13:37 +0900

    update

--- R/main.R
+++ R/main.R
@@ -2,27 +2,28 @@
 #' @export
 #' @param api_key API Key object (see also, setapi())
 #' @param compnum company number (see also, compdata)
-#' @param pagination If pagination is TRUE, all data are retrieved.
-get_mer_data<-function(api_key,compnum,pagination=FALSE) {
+get_mer_data<-function(api_key,compnum) {
   if(!setapi(api_key)) {
     return(NULL)
   }
-  Quandl::Quandl.database('MER/F1',compnumber=as.character(compnum),pagination=pagination)%>%
+  Quandl.datatable('MER/F1',compnumber=as.character(compnum))%>%
     as_tibble()
 }
 
 #' API setup
 #'
+#' @export
 #' @param Quandl 홈페이지에서 받은 api_key입니다.
 setapi <- function(your_api_key=NULL) {
   if(is.null(your_api_key)) {
     cat("Visit https://www.quandl.com/ to get an API key.\n")
     return(FALSE)
   }
-  Quandly::Quandl.api_key(your_api_key)
+  Quandl.api_key(your_api_key)
   return(TRUE)
 }
-
+#' Test Key
+#' @export
 test_key<-function() {
   return("xnwKJ-nJVgnVt2JEDpf2")
 }
